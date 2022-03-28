cd8bc493889c928742764f0924b38fb28fcaeea2
BRAKEL3SS <rck496@yahoo.com>
2022-03-28 11:32:19 -0700

    Added index functionality

--- models/team.js
+++ models/team.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema
 
-const teamSchema = new Schema({
-    name: String,
-    location: String,
-    drivers: [driverSchema]
-}, {
-    timestamps: true
-})
-
 const driverSchema = new Schema({
     name: String,
     age: Number,
     homeCountry: String
+}, {
+    timestamps: true
+})
+
+const teamSchema = new Schema({
+    name: String,
+    location: String,
+    drivers: [driverSchema]
 }, {
     timestamps: true
 })
