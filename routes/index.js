ffd96c8f470b6f6833d888f14124fb8b85e67823
BRAKEL3SS <rck496@yahoo.com>
2022-03-29 11:58:26 -0700

    Added OAuth, started implementing authorization

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+//Google Oauth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email']
+    //optionally force pick account every time
+    //prompt: "select_account"
+  }
+))
+
+// Google Oauth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/teams', 
+    failureRedirect: '/teams'
+  }
+))
+
+//oauth logout route
+router.get('/logout', function(req,res){
+  req.logout()
+  res.redirect('/teams')
+})
 
 module.exports = router;
