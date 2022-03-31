dde78776064f0c5d2de57c21b87f6adea1ffe563
BRAKEL3SS <rck496@yahoo.com>
2022-03-31 09:18:16 -0700

    styling updates

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {title: 'Welcome to Formula 1'});
 });
 
 //Google Oauth login route
